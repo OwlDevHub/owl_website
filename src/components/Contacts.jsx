4b85e23a1e62768ba3aa1ffe02a3fe46d35d7d3a
Nighty3098 <night3098games@gmail.com>
2025-02-08 01:04:25 +0700

    Switching to react

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTelegram, faDiscord, faReddit, faSignalMessenger } from '@fortawesome/free-brands-svg-icons'; // Импортируем иконки
+
+const socials = [
+  { icon: faTelegram, link: 'https://t.me/Night3098' },
+  { icon: faDiscord, link: 'https://discord.gg/#9707' },
+  { icon: faReddit, link: 'https://www.reddit.com/user/DEVELOPER0x31/' },
+  { icon: faSignalMessenger, link: 'https://signal.me/#eu/XJMqmO9JXZQCwYJIpzjOS741ZnGsLYOQhGqMfpS4lB-8PTSQVmRAbqFIvOrepYiK' },
+];
+
+const Contacts = () => (
+  <div className="contacts" id="contacts">
+    <h2>Contact with me</h2>
+    <div className="contacts_badge_block">
+      {socials.map((social, index) => (
+        <a key={index} href={social.link} target="_blank" rel="noopener noreferrer">
+          <div className="contact_button">
+            <i>
+              <FontAwesomeIcon icon={social.icon} className="social-icon" />
+            </i>
+          </div>
+        </a>
+      ))}
+    </div>
+    <a href="https://discord.gg/tnHSEc2cZv" className="btn-slide2">
+      <span className="circle2"><FontAwesomeIcon icon={faDiscord} /></span>
+      <span className="title2">DS SERVER</span>
+      <span className="title-hover2">CLICK</span>
+    </a>
+  </div>
+);
+
+export default Contacts;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <p className="rights animate__animated animate__backInUp">
+      &copy; <span id="year"></span> Semenov Artem (Nighty3098). All Rights Reserved.
+    </p>
+  </div>
+);
+
+export default Footer;
