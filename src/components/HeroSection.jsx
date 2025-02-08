12baa3b4a2b490970edf30a5cf5bdc278de6ef54
Nighty3098 <night3098games@gmail.com>
2025-02-08 19:53:55 +0700

    Working on new version

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import OwlIcon from './OwlIcon';
 
 export const HeroSection = () => {
   return (
-      <div className="main_block animate__animated animate__bounceInDown" id="about_app">
+      <div className="main_block animate__animated animate__fadeInDown" id="about_app">
         <OwlIcon />
         <h1>Modern task and project manager<br />for your productivity</h1>
       </div>
@@ -13,7 +13,7 @@ export const HeroSection = () => {
 export const SloganSection = ()  => {
   return (
     <div className="main_block" style={{ textAlign: "left", height: "100%", width: "100%"}}>
-      <h1 className='big_text'><p style={{ padding: "0px", margin: "0px", fontSize: "xxx-large", fontWeight: "1000"}}>OWL:</p>Your ideas<br />your organization<br />your freedom</h1>
+      <h1 className='big_text'><p style={{ padding: "0px", paddingBottom: "20px", margin: "0px", fontStyle: "italic", fontSize: "xx-large", fontWeight: "1000"}}>OWL:</p>- Your ideas<br />- your organization<br />- your freedom</h1>
     </div>
   )
 }
