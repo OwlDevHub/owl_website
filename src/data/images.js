7e9795b73c76bf426e573157fdd2132e6c384af5
Nighty3098 <night3098games@gmail.com>
2025-03-19 19:53:53 +0700

    Update

---
+++ src/data/images.js
@@ -0,0 +1,15 @@
+const images = [
+    {
+      imgURL:
+        "/imgs/app_1.png",
+      imgAlt: "img-1"
+    },
+    {
+      imgURL:
+        "/imgs/app_2.png",
+      imgAlt: "img-2"
+    }
+  ];
+  
+  export default images;
+  
