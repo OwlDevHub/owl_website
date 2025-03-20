78e110c46a1ba4971dbd239e83cc772e9c088a09
Nighty3098 <night3098games@gmail.com>
2025-03-20 20:33:46 +0700

    chore: add project screenshots

--- src/data/images.js
+++ src/data/images.js
@@ -1,12 +1,32 @@
 const images = [
   {
-    imgURL: "/imgs/app_1.png",
+    imgURL: "/imgs/1.png",
     imgAlt: "img-1",
   },
   {
-    imgURL: "/imgs/app_2.png",
+    imgURL: "/imgs/2.png",
     imgAlt: "img-2",
   },
+  {
+    imgURL: "/imgs/3.png",
+    imgAlt: "img-3",
+  },
+  {
+    imgURL: "/imgs/4.png",
+    imgAlt: "img-4",
+  },
+  {
+    imgURL: "/imgs/5.png",
+    imgAlt: "img-5",
+  },
+  {
+    imgURL: "/imgs/6.png",
+    imgAlt: "img-6",
+  },
+  {
+    imgURL: "/imgs/7.png",
+    imgAlt: "img-7",
+  }
 ];
 
 export default images;
