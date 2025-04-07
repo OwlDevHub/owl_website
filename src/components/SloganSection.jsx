3d82c1f632d0ca95ded1d729df524a3fc51a80d9
Nighty3098 <night3098games@gmail.com>
2025-04-07 18:58:13 +0700

    Update

--- src/components/SloganSection.jsx
+++ src/components/SloganSection.jsx
@@ -13,67 +13,43 @@ export const SloganSection = () => {
   const isInView = useInView(ref, { once: true });
 
   return (
-    <>
+    <div className="dock">
       <motion.div
-        className="float-widget"
+        className="dock-item"
+        style={{ backgroundColor: "var(--yellow)"}}
         ref={ref}
-        style={{
-          top: "10%",
-          right: "8%",
-          rotate: "-15deg",
-        }}
-        initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : -50 }}
         transition={{ duration: 0.5 }}
       >
         <FontAwesomeIcon className="icon" icon={faLightbulb} />
-        <p>ideas</p>
       </motion.div>
       <motion.div
-        className="float-widget"
+        className="dock-item"
+        style={{ backgroundColor: "var(--red)"}}
         ref={ref}
-        style={{
-          top: "15%",
-          left: "10%",
-          rotate: "10deg",
-        }}
-        initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : -50 }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
         <FontAwesomeIcon className="icon" icon={faListCheck} />
-        <p>organization</p>
       </motion.div>
       <motion.div
-        className="float-widget"
+        className="dock-item"
+        style={{ backgroundColor: "var(--purple)"}}
         ref={ref}
-        style={{
-          bottom: "19%",
-          right: "13%",
-          rotate: "-12deg",
-        }}
-        initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
         transition={{ duration: 0.5, delay: 0.4 }}
       >
         <FontAwesomeIcon className="icon" icon={faFeather} />
-        <p>freedom</p>
       </motion.div>
       <motion.div
-        className="float-widget"
+        className="dock-item"
+        style={{ backgroundColor: "var(--green)"}}
         ref={ref}
-        style={{
-          bottom: "14%",
-          left: "20%",
-          rotate: "16deg",
-        }}
-        initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 50 }}
         transition={{ duration: 0.5, delay: 0.4 }}
       >
         <FontAwesomeIcon className="icon" icon={faClock} />
-        <p>productivity</p>
       </motion.div>
-    </>
+    </div>
   );
 };
