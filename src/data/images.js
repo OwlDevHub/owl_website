5f39065f2eae977e869424c5be00c63276a5c186
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-10-06 17:56:46 +0700

    chore: update images and refactor ImageGallery

--- src/data/images.js
+++ src/data/images.js
@@ -30,7 +30,15 @@ const images = [
   {
     imgURL: "/imgs/8.png",
     imgAlt: "img-8",
-  }
+  },
+  {
+    imgURL: "/imgs/9.png",
+    imgAlt: "img-9",
+  },
+  {
+    imgURL: "/imgs/10.png",
+    imgAlt: "img-10",
+  },
 ];
 
 export default images;
