9ef68a64e08d88ddec3e69eeefb09e4f249d5a5f
Artem <154594695+Nighty3098@users.noreply.github.com>
2026-03-10 11:06:43 +0700

    chore: update copyright year to 2026-2027

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <div className="footer">
     <a href="https://nighty3098.vercel.app/" style={{ color: "var(--green)" }}>
-      &copy; 2025 Semyonov Artyom (Nighty3098).
+      &copy; 2026-2027 (Nighty3098).
     </a>
     <br />
     All Rights Reserved.
